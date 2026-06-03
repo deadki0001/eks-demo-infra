fc75ac5cf91644c8f4297c8ef30ba4ce245acf5c
Devon Adkins <adkins.devon@hotmail.com>
2026-06-03 12:24:47 +0200

    fix: add bootstrap_cluster_creator_admin_permissions to access_config

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -83,7 +83,8 @@ resource "aws_eks_cluster" "main" {
   version  = "1.30"
 
   access_config {
-    authentication_mode = "API_AND_CONFIG_MAP"
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
   }
 
   vpc_config {
